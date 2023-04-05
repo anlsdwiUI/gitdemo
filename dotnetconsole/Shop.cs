4cec0c0af2669827d1000d1ee9cfa833b855c0d7
anlsdwiUI <analistiana.dewi@ui.ac.id>
2023-04-05 09:56:25 +0700

    "tambah"

---
+++ dotnetconsole/Shop.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Shop
+{
+	public string Name { get; set; }
+	public string Description { get; set; }
+	public string ShopId { get; set; }
+	public int ShopCount { get; set; }
+}
